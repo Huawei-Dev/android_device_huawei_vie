c54477c05e51505c7570c111bca4e3f71646fe82
Surdu Petru <surdu_petru@yahoo.com>
2016-09-05 20:42:31 +0200

    right path to fix compiling

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -53,7 +53,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.connectivity.gps.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := init.connectivity.gps.rc
+LOCAL_SRC_FILES    := etc/init.connectivity.gps.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
